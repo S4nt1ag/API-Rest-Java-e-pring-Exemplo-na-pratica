b06eec70389b060d9a55b9d243aef5dbd71c47dc
Santiago <gabrielsantiagoneves2@gmail.com>
2023-05-09 17:38:59 -0300

    aplicação de banco de dados

--- demo/src/main/java/com/Santiago/demo/entities/Department.java
+++ demo/src/main/java/com/Santiago/demo/entities/Department.java
@@ -1,6 +1,17 @@
 package com.Santiago.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_departament")
 public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
